8b8e59ac11b4f55e342a7aa2378df9133a5f9986
cooooong <czhao3133@outlook.com>
2025-09-20 18:39:59 +0800

    bug fixed

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from astrbot.api import logger
 from astrbot.api import AstrBotConfig
 from zai import ZhipuAiClient
 
-@register("bmtxt2img", "CoooooNG", "智谱文生图插件", "1.0.0")
+@register("bmtxt2img", "CoooooNG", "智谱文生图插件", "1.0.0", "https://github.com/CoooooNG/astrbot_plugin_bmtxt2img")
 class MyPlugin(Star):
     def __init__(self, context: Context, config: AstrBotConfig):
         super().__init__(context)
@@ -15,10 +15,11 @@ class MyPlugin(Star):
     
     @filter.command("bmimg")
     async def bmtxt2img(self, event: AstrMessageEvent):
-
+        '''这是一个文生图指令'''
         user_name = event.get_sender_name()
         message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
+
+        logger.info("调用智谱文生图api")
 
         # Initialize client
         client = ZhipuAiClient(api_key=self.config.token)
@@ -32,9 +33,7 @@ class MyPlugin(Star):
         )
 
         image_url = response.data[0].url
-
-        logger.info(message_chain)
-        yield event.image_result({image_url})
+        yield event.image_result(image_url)
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
